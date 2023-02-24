2cec8c27d401637c9d528ff8045b169ab9aff2a3
homyhan <homyhan0616@gmail.com>
2023-02-24 21:03:05 +0700

    initialize project

---
+++ src/app/store.js
@@ -0,0 +1,11 @@
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const reducer = combineReducers({
+    
+  });
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+export default store;
