b07cbb47bb547e777377030fed512218200d9de4
homyhan <homyhan0616@gmail.com>
2023-03-26 12:07:10 +0700

    update getLoaiNguoiDung

--- src/features/Admin/components/AddUser.jsx
+++ src/features/Admin/components/AddUser.jsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from "react";
 import * as Yup from "yup";
-import {
-  Button,
-  Form,
-  Input,
-  message,
-  Select,
-} from "antd";
+import { Button, Form, Input, message, Select } from "antd";
 
 import LayoutAdmin from "../../../HOCs/LayoutAdmin";
 import { useFormik } from "formik";
@@ -15,7 +9,7 @@ import { addUser } from "../thunk";
 import { useNavigate } from "react-router-dom";
 import { movieList } from "../services/adminService";
 import { createBrowserHistory } from "history";
-import './User.css';
+import "./User.css";
 let history = createBrowserHistory();
 
 const AddUser = () => {
@@ -23,22 +17,35 @@ const AddUser = () => {
     navigate("/" + page);
   };
   const [state, setState] = useState({
-    loaiNguoiDung: []
-  })
-  useEffect(
-    () => async () => {
-      try {
-        const res = await movieList.getLoaiNguoiDung();
-        setState({
-          ...state,
-          loaiNguoiDung: res.data.content,
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    []
-  );
+    loaiNguoiDung: [],
+  });
+  const fetchData = () => {
+    movieList.getLoaiNguoiDung()
+    .then((res) => {
+      setState({
+        ...state,
+        loaiNguoiDung: res.data.content,
+      });
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  };
+  useEffect(()=>fetchData(), []);
+  // useEffect(
+  //   () => async () => {
+  //     try {
+  //       const res = await movieList.getLoaiNguoiDung();
+  //       setState({
+  //         ...state,
+  //         loaiNguoiDung: res.data.content,
+  //       });
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   },
+  //   []
+  // );
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const idGroup = JSON.parse(localStorage.getItem("USER_LOGIN")).maNhom;
@@ -53,67 +60,70 @@ const AddUser = () => {
       hoTen: "",
     },
     validationSchema: Yup.object().shape({
-      taiKhoan: Yup.string().trim().required("Required").matches(
-        /([A-z]\S)+/g,
-        "Account mustn't whitespace"
-      ),
-      matKhau: Yup.string().required("Required").matches(
-        /^(?=.*[a-z]*)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[!@#\$%\^&\*]+).{6,10}$/g,
-        "Mật Khẩu từ 6-10 ký tự (chứa ít nhất 1 ký tự số, 1 ký tự in hoa, 1 ký tự đặc biệt)"
-      ),
-      email: Yup.string().required("Required").matches(
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g,
-        "Error Email"
-      ),
-      soDt: Yup.string().required("Required").matches(
-        /(^[0-9]{10}$)+/g,
-        "Error Phone"
-      ),
-      hoTen: Yup.string().trim().required("Required").matches(
-        /^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]+$/g,
-        "Error Fullname"
-      ),
-      maLoaiNguoiDung: Yup.string().required('Vui lòng chọn tùy chọn')
-
+      taiKhoan: Yup.string()
+        .trim()
+        .required("Required")
+        .matches(/([A-z]\S)+/g, "Account mustn't whitespace"),
+      matKhau: Yup.string()
+        .required("Required")
+        .matches(
+          /^(?=.*[a-z]*)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[!@#\$%\^&\*]+).{6,10}$/g,
+          "Mật Khẩu từ 6-10 ký tự (chứa ít nhất 1 ký tự số, 1 ký tự in hoa, 1 ký tự đặc biệt)"
+        ),
+      email: Yup.string()
+        .required("Required")
+        .matches(
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g,
+          "Error Email"
+        ),
+      soDt: Yup.string()
+        .required("Required")
+        .matches(/(^[0-9]{10}$)+/g, "Error Phone"),
+      hoTen: Yup.string()
+        .trim()
+        .required("Required")
+        .matches(
+          /^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]+$/g,
+          "Error Fullname"
+        ),
+      maLoaiNguoiDung: Yup.string().required("Vui lòng chọn tùy chọn"),
     }),
     validateOnBlur: true,
     validateOnChange: true,
     onSubmit: async (values) => {
       await dispatch(addUser(values));
-      history.back()
+      history.back();
       console.log("values", values);
     },
   });
 
   const handleChangeDoiTuong = (values) => {
     console.log(values);
-   return formik.setFieldValue("maLoaiNguoiDung", values);
+    return formik.setFieldValue("maLoaiNguoiDung", values);
   };
- 
+
   return (
     <LayoutAdmin>
-      
       <div className="top2btn">
         <div className="main2btn">
-        <Button
-          className="w-full mb-2 rounded-none"
-          onClick={() => {
-            redirect("admin");
-          }}
-        >
-          Movie Manager
-        </Button>
-        
-        <Button
-          onClick={() => {
-            redirect("admin/user");
-          }}
-          className="w-full rounded-none mb-2"
-        >
-          User Manager
-        </Button>
+          <Button
+            className="w-full mb-2 rounded-none"
+            onClick={() => {
+              redirect("admin");
+            }}
+          >
+            Movie Manager
+          </Button>
+
+          <Button
+            onClick={() => {
+              redirect("admin/user");
+            }}
+            className="w-full rounded-none mb-2"
+          >
+            User Manager
+          </Button>
         </div>
-        
       </div>
       <h3 className="px-3">Add User</h3>
       <Form
@@ -135,22 +145,37 @@ const AddUser = () => {
         }}
       >
         <Form.Item label="Account">
-          <Input placeholder="Account" name="taiKhoan" onBlur={formik.handleBlur} onChange={formik.handleChange} />
-          {formik.touched.taiKhoan &&formik.errors.taiKhoan  ? (
+          <Input
+            placeholder="Account"
+            name="taiKhoan"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.taiKhoan && formik.errors.taiKhoan ? (
             <p className="text-red-600 font-bold">{formik.errors.taiKhoan}</p>
-          ) : null }
+          ) : null}
         </Form.Item>
         <Form.Item label="Fullname">
-          <Input placeholder="Fullname" name="hoTen" onBlur={formik.handleBlur} onChange={formik.handleChange} />
-          {formik.touched.hoTen &&formik.errors.hoTen  ? (
+          <Input
+            placeholder="Fullname"
+            name="hoTen"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.hoTen && formik.errors.hoTen ? (
             <p className="text-red-600 font-bold">{formik.errors.hoTen}</p>
-          ) : null }
+          ) : null}
         </Form.Item>
         <Form.Item label="Password">
-          <Input placeholder="Password" name="matKhau" onBlur={formik.handleBlur} onChange={formik.handleChange} />
-          {formik.touched.matKhau &&formik.errors.matKhau  ? (
+          <Input
+            placeholder="Password"
+            name="matKhau"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.matKhau && formik.errors.matKhau ? (
             <p className="text-red-600 font-bold">{formik.errors.matKhau}</p>
-          ) : null }
+          ) : null}
         </Form.Item>
         <Form.Item label="Email">
           <Input
@@ -159,30 +184,36 @@ const AddUser = () => {
             onBlur={formik.handleBlur}
             placeholder="Email"
           />
-          {formik.touched.email &&formik.errors.email  ? (
+          {formik.touched.email && formik.errors.email ? (
             <p className="text-red-600 font-bold">{formik.errors.email}</p>
-          ) : null }
+          ) : null}
         </Form.Item>
         <Form.Item label="Phone">
-          <Input name="soDt" placeholder="Phone" onBlur={formik.handleBlur} onChange={formik.handleChange} />
-          {formik.touched.soDt &&formik.errors.soDt  ? (
+          <Input
+            name="soDt"
+            placeholder="Phone"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.soDt && formik.errors.soDt ? (
             <p className="text-red-600 font-bold">{formik.errors.soDt}</p>
-          ) : null }
+          ) : null}
         </Form.Item>
-
 
         <Form.Item label="Permission">
           <Select
-            options={state.loaiNguoiDung?.map((item, index)=>({
+            options={state.loaiNguoiDung?.map((item, index) => ({
               label: item.tenLoai,
               value: item.maLoaiNguoiDung,
             }))}
             onChange={handleChangeDoiTuong}
-            onBlur={() => formik.setFieldTouched('select', true)}
+            onBlur={() => formik.setFieldTouched("select", true)}
             placeholder="Permission"
           />
           {formik.touched.maLoaiNguoiDung && formik.errors.maLoaiNguoiDung ? (
-            <p className="text-red-600 font-bold">{formik.errors.maLoaiNguoiDung}</p>
+            <p className="text-red-600 font-bold">
+              {formik.errors.maLoaiNguoiDung}
+            </p>
           ) : null}
         </Form.Item>
 
@@ -208,5 +239,3 @@ const AddUser = () => {
 };
 
 export default AddUser;
-
-
